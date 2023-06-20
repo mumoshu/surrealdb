decfa5f4bfba6ad74e3fb8eb387d0e5ed4672261
Yusuke Kuoka <ykuoka@gmail.com>
2023-06-20 05:19:43 +0000

    Format test code

--- lib/tests/api/mod.rs
+++ lib/tests/api/mod.rs
@@ -671,7 +671,6 @@ async fn delete_record_range() {
 	);
 }
 
-
 #[tokio::test]
 async fn changefeed() {
 	let db = new_db().await;
@@ -680,21 +679,17 @@ async fn changefeed() {
 	let sql = "
 	DEFINE TABLE user CHANGEFEED 1h;
 	";
-	let response = db.query(sql)
-		.await
-		.unwrap();
+	let response = db.query(sql).await.unwrap();
 	response.check().unwrap();
 	// Create and update users
-    let sql = "
+	let sql = "
         CREATE user:amos SET name = 'Amos';
         CREATE user:jane SET name = 'Jane';
         UPDATE user:amos SET name = 'AMOS';
     ";
 	let table = "user";
-    let response = db.query(sql)
-        .await
-        .unwrap();
-    response.check().unwrap();
+	let response = db.query(sql).await.unwrap();
+	response.check().unwrap();
 	let users: Vec<RecordBuf> = db
 		.update(table)
 		.content(Record {
@@ -702,29 +697,24 @@ async fn changefeed() {
 		})
 		.await
 		.unwrap();
-    let expected = &[
-        RecordBuf {
-            id: thing("user:amos").unwrap(),
-            name: "Doe".to_owned(),
-        },
-        RecordBuf {
-            id: thing("user:jane").unwrap(),
-            name: "Doe".to_owned(),
-        },
-    ];
-    assert_eq!(users, expected);
-	let users: Vec<RecordBuf> = db
-		.select(table)
-		.await
-		.unwrap();
-    assert_eq!(users, expected);
-    let sql = "
+	let expected = &[
+		RecordBuf {
+			id: thing("user:amos").unwrap(),
+			name: "Doe".to_owned(),
+		},
+		RecordBuf {
+			id: thing("user:jane").unwrap(),
+			name: "Doe".to_owned(),
+		},
+	];
+	assert_eq!(users, expected);
+	let users: Vec<RecordBuf> = db.select(table).await.unwrap();
+	assert_eq!(users, expected);
+	let sql = "
         SHOW CHANGES FOR TABLE user SINCE 0 LIMIT 10;
     ";
-    let mut response = db.query(sql)
-        .await
-        .unwrap();
-    let value: Value = response.take(0).unwrap();
+	let mut response = db.query(sql).await.unwrap();
+	let value: Value = response.take(0).unwrap();
 	let Value::Array(array) = value.clone() else { unreachable!() };
 	assert_eq!(array.len(), 4);
 	// UPDATE user:amos
@@ -732,49 +722,70 @@ async fn changefeed() {
 	let Value::Object(a) = a else { unreachable!() };
 	let Value::Number(versionstamp1) = a.get("versionstamp").unwrap() else { unreachable!() };
 	let changes = a.get("changes").unwrap().to_owned();
-	assert_eq!(changes, surrealdb::sql::value("[
+	assert_eq!(
+		changes,
+		surrealdb::sql::value(
+			"[
 		{
 			update: {
 				id: user:amos,
 				name: 'Amos'
 			}
 		}
-	]").unwrap());
+	]"
+		)
+		.unwrap()
+	);
 	// UPDATE user:jane
 	let a = array.get(1).unwrap();
 	let Value::Object(a) = a else { unreachable!() };
 	let Value::Number(versionstamp2) = a.get("versionstamp").unwrap() else { unreachable!() };
 	assert!(versionstamp1 < versionstamp2);
 	let changes = a.get("changes").unwrap().to_owned();
-	assert_eq!(changes, surrealdb::sql::value("[
+	assert_eq!(
+		changes,
+		surrealdb::sql::value(
+			"[
 		{
 			update: {
 				id: user:jane,
 				name: 'Jane'
 			}
 		}
-	]").unwrap());
+	]"
+		)
+		.unwrap()
+	);
 	// UPDATE user:amos
 	let a = array.get(2).unwrap();
 	let Value::Object(a) = a else { unreachable!() };
 	let Value::Number(versionstamp3) = a.get("versionstamp").unwrap() else { unreachable!() };
 	assert!(versionstamp2 < versionstamp3);
 	let changes = a.get("changes").unwrap().to_owned();
-	assert_eq!(changes, surrealdb::sql::value("[
+	assert_eq!(
+		changes,
+		surrealdb::sql::value(
+			"[
 		{
 			update: {
 				id: user:amos,
 				name: 'AMOS'
 			}
 		}
-	]").unwrap());
+	]"
+		)
+		.unwrap()
+	);
 	// UPDATE table
 	let a = array.get(3).unwrap();
 	let Value::Object(a) = a else { unreachable!() };
 	let Value::Number(versionstamp4) = a.get("versionstamp").unwrap() else { unreachable!() };
 	assert!(versionstamp3 < versionstamp4);
 	let changes = a.get("changes").unwrap().to_owned();
-	assert_eq!(changes, surrealdb::sql::value("[
+	assert_eq!(
+		changes,
+		surrealdb::sql::value(
+			"[
 		{
 			update: {
 				id: user:amos,
@@ -787,7 +798,10 @@ async fn changefeed() {
 				name: 'Doe'
 			}
 		}
-	]").unwrap());
+	]"
+		)
+		.unwrap()
+	);
 }
 
 #[tokio::test]
